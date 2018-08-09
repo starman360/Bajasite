5f5269d089d2610ea52c8ded87d5a5c8a9230cd7
Unknown <anmol.modur@gmail.com>
2018-08-09 16:04:18 -0700

    Added Sponsors

---
+++ assets/js/carparts.js
@@ -0,0 +1,18 @@
+var carparts = [
+    {
+        "title": "Steering",
+        "loc": {"top":"35%",
+        "left":"39%","width":"30","height":"30"},
+        "designlead": "Alex Smart",
+        "leadpic": "assets/images/team/alex_smart.jpg",
+        "desc":"The Steering component is what makes the car steer through obstacles, maneuver through tight spaces and slide around corners."
+    },
+    {
+        "title": "Suspension",
+        "loc": {"top":"48%",
+        "left":"31%","width":"30","height":"30"},
+        "designlead": "Asif Habib",
+        "leadpic": "assets/images/team/alex_smart.jpg",
+        "desc": "The Suspension"
+    }
+];
