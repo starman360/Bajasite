c87f4d7c544585ff2dc263165a5cfe8e272402d8
Unknown <anmol.modur@gmail.com>
2018-09-30 13:09:39 -0400

    Added Gallery

---
+++ assets/js/team.js
@@ -0,0 +1,39 @@
+// Images for team members will be in /assets/images/team/---.jpg
+// file name will be full name all lowercase and _ for spaces.
+// Alex Smart -> alex_smart.jpg
+
+var teamMembers = {
+    "Eboard":[
+        {
+            "title":"Team Manager",
+            "name":"Chris Mehalakes",
+            "email":"teammanager@ritbaja.com",
+            "major":"Mechanical Engineering Tech",
+            "gradYear":"2019"
+        },
+        {
+            "title":"Project Manager",
+            "name":"Justin Neves",
+            "email":"projectmanager@ritbaja.com",
+            "major":"Civil Engineering Tech",
+            "gradYear":"2020"
+        },
+        {
+            "title":"Treasurer",
+            "name":"Adam Seidman",
+            "email":"treasurer@ritbaja.com",
+            "major":"Computer Engineering",
+            "gradYear":"2022"
+        },
+        {
+            "title":"Secretary",
+            "name":"Colton Johnson",
+            "email":"secretary@ritbaja.com",
+            "major":"Mechanical Engineering",
+            "gradYear":"2020"
+        }
+    ],
+    "DesignTeam":[],
+    "Manufacturing":[],
+    "Seniors":[]
+};
