300e0f8ca492300f73d19ff5d40615fde5c747ce
Unknown <anmol.modur@gmail.com>
2018-10-27 11:39:30 -0400

    Added all updates to Team and more

--- assets/js/team.js
+++ assets/js/team.js
@@ -37,3 +37,34 @@ var teamMembers = {
     "Manufacturing":[],
     "Seniors":[]
 };
+
+function createTeamTiles(year){
+
+    function hextile(img, index) {
+        return '<li class="hex"><div class="hexIn" data-toggle="modal" data-target="#gallery' + index + '"><a class="hexLink" href="#"><img src="' + img.imageloc + '" alt="" /><h1>' + img.title + '</h1><p>' + img.desc + '</p></a></div></li>'
+    }
+
+    function modaltile(img, index) {
+        return '<div class="modal fade" id="gallery' + index + '" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true"><div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">' + img.title + '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><img src="' + img.imageloc + '" width="100%"><div class="modal-body"><p>' + img.desc + '</p></div></div></div></div>'
+    }
+
+    gallery[year].forEach(function (img, i) {
+        var hex = hextile(img, i);
+        var popup = modaltile(img, i);
+        $('ul#hexGrid').append(hex);
+        $('div#gallery-hex').append(popup);
+    });
+}
+
+$(document).ready(function(){
+    $('h1.group').each(function(value, i){
+        $(this).css('top', $(this).width() + 106);
+    });
+    
+    // var file = window.document.location.href.split("/");
+    // file = file[file.length - 1].split('#');
+    // year = file[0].split('=');
+    // if (year.length == 1) year = 18;
+    // else year = parseInt(year[1]);
+    // createGalleryTiles(year + 2000);
+});
